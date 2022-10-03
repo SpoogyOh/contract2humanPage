6ceff329e85a81918dd7699a4d552aba65d8fecb
SpoogyOH <opiola.daniel@gmail.com>
2022-10-03 21:54:24 +0200

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,14 +15,14 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-})
-module.exports = {
+
   images: {
     unoptimized: true,
   },
-};
+});
