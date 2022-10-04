0f384ec5f6cad5909cef60ab915b337705269b5a
SpoogyOH <opiola.daniel@gmail.com>
2022-10-04 09:14:08 +0200

    modified:   src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <title>Contract to Human</title>
         <meta name="description" content="Veträge analysieren" />
         <link rel="icon" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Navbar />
       <Main />
