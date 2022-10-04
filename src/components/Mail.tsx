38aa5618a56d21f704a0488c5ee545c1115f66ef
SpoogyOH <opiola.daniel@gmail.com>
2022-10-04 10:36:13 +0200

    modified:   src/components/Mail.tsx

--- src/components/Mail.tsx
+++ src/components/Mail.tsx
@@ -26,18 +26,18 @@ const Mail = () => {
           <div className=" w-full h-auto rounded-xl lg:p-4">
             <div className="p-4">
               <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-row justify-center  gap-4 w-full py-2">
+                <div className="flex flex-row justify-center gap-4">
                   <div className="flex flex-col">
                     <div className="flex flex-row">
                       <input
-                        className="border-2 rounded-lg p-3 flex border-gray-300"
+                        className="border-2 rounded-lg flex border-gray-300"
                         type="text"
                         placeholder="Vorname"
                         {...register("firstName")}
                       />
                       <div className="pr-2"></div>
                       <input
-                        className="border-2 rounded-lg p-3 flex border-gray-300"
+                        className="border-2 rounded-lg  flex border-gray-300"
                         type="text"
                         placeholder="Nachname"
                         {...register("lastName")}
@@ -45,14 +45,14 @@ const Mail = () => {
                     </div>
                     <div className="pt-4 " />
                     <input
-                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      className="border-2 rounded-lg  flex border-gray-300"
                       type="text"
                       placeholder="E-Mail"
                       {...register("email")}
                     />
                     <div className="pt-4 content-center" />
                     <button
-                      className="border-2 rounded-lg p-3 flex border-gray-300 justify-center"
+                      className="border-2 rounded-lg flex border-gray-300 justify-center"
                       disabled={isSubmitSuccessful}
                     >
                       {isSubmitSuccessful ? "Danke!" : "Trag dich ein."}
