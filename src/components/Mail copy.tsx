d00570fc38deaf8d944154ba97b0b3217acbed24
SpoogyOH <opiola.daniel@gmail.com>
2022-10-04 11:19:54 +0200

    new file:   src/components/Mail copy.tsx 	modified:   src/components/Mail.tsx

---
+++ src/components/Mail copy.tsx
@@ -0,0 +1,71 @@
+import { useForm } from "react-hook-form";
+import { CreateMailInput } from "../types/mailSchema";
+import { trpc } from "../utils/trpc";
+
+const Mail = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitSuccessful },
+  } = useForm<CreateMailInput>();
+
+  const { mutate } = trpc.useMutation(["mail.createMail"], {});
+
+  function onSubmit(values: CreateMailInput) {
+    mutate(values);
+  }
+
+  return (
+    <div id="waitlist" className="w-full text-center bg-white">
+      <div className="max-w-[1240px] m-2 py-16 w-full ">
+        <h2 className="py-4">Möchtest du es ausprobieren?</h2>
+        <p className="text-xl tracking-widest uppercase text-red">
+          Trag dich in die Warteliste ein!
+        </p>
+        <div className="">
+          <div className=" w-full h-auto rounded-xl lg:p-4">
+            <div className="p-4">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-row justify-center gap-4">
+                  <div className="flex flex-col">
+                    <div className="flex flex-row">
+                      <input
+                        className="border-2 rounded-lg flex md:p-3 border-gray-300"
+                        type="text"
+                        placeholder="Vorname"
+                        {...register("firstName")}
+                      />
+                      <div className="pr-2"></div>
+                      <input
+                        className="border-2 rounded-lg md:p-3 flex border-gray-300"
+                        type="text"
+                        placeholder="Nachname"
+                        {...register("lastName")}
+                      />
+                    </div>
+                    <div className="pt-4 " />
+                    <input
+                      className="border-2 rounded-lg md:p-3 flex border-gray-300"
+                      type="text"
+                      placeholder="E-Mail"
+                      {...register("email")}
+                    />
+                    <div className="pt-4 content-center" />
+                    <button
+                      className="border-2 rounded-lg flex md:p-3 border-gray-300 justify-center"
+                      disabled={isSubmitSuccessful}
+                    >
+                      {isSubmitSuccessful ? "Danke!" : "Trag dich ein."}
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mail;
--- src/components/Mail.tsx
+++ src/components/Mail.tsx
@@ -16,51 +16,48 @@ const Mail = () => {
   }
 
   return (
-    <div id="waitlist" className="w-full text-center bg-white">
-      <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
-        <h2 className="py-4">Möchtest du es ausprobieren?</h2>
-        <p className="text-xl tracking-widest uppercase text-red">
+    <div id="waitlist" className="w-full text-center bg-white ">
+      <div className=" m-2 ">
+        <h2>Möchtest du es ausprobieren?</h2>
+        <p className="md:text-xl widest uppercase text-red">
           Trag dich in die Warteliste ein!
         </p>
-        <div className="">
-          <div className=" w-full h-auto rounded-xl lg:p-4">
-            <div className="p-4">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-row justify-center gap-4">
-                  <div className="flex flex-col">
-                    <div className="flex flex-row">
-                      <input
-                        className="border-2 rounded-lg flex md:p-3 border-gray-300"
-                        type="text"
-                        placeholder="Vorname"
-                        {...register("firstName")}
-                      />
-                      <div className="pr-2"></div>
-                      <input
-                        className="border-2 rounded-lg md:p-3 flex border-gray-300"
-                        type="text"
-                        placeholder="Nachname"
-                        {...register("lastName")}
-                      />
-                    </div>
-                    <div className="pt-4 " />
+      </div>
+      <div>
+        <div>
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-row justify-center">
+                <div className="flex flex-col m-2">
+                  <div className="flex gap-2 flex-row m-2 ">
                     <input
-                      className="border-2 rounded-lg md:p-3 flex border-gray-300"
+                      className="border-2 rounded-lg border-gray-300 md:p-3"
                       type="text"
-                      placeholder="E-Mail"
-                      {...register("email")}
+                      placeholder="Vorname"
+                      {...register("firstName")}
                     />
-                    <div className="pt-4 content-center" />
-                    <button
-                      className="border-2 rounded-lg flex md:p-3 border-gray-300 justify-center"
-                      disabled={isSubmitSuccessful}
-                    >
-                      {isSubmitSuccessful ? "Danke!" : "Trag dich ein."}
-                    </button>
+                    <input
+                      className=" border-2 rounded-lg md:p-3 flex border-gray-300"
+                      type="text"
+                      placeholder="Nachname"
+                      {...register("lastName")}
+                    />
                   </div>
+                  <input
+                    className="border-2 rounded-lg m-2 md:p-3 flex border-gray-300"
+                    type="text"
+                    placeholder="E-Mail"
+                    {...register("email")}
+                  />
+                  <button
+                    className="border-2 rounded-lg flex m-2 md:p-3 border-gray-300 justify-center"
+                    disabled={isSubmitSuccessful}
+                  >
+                    {isSubmitSuccessful ? "Danke!" : "Trag dich ein."}
+                  </button>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
